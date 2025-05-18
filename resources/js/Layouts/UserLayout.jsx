f93c4668abdc41b71c11093aafab8600d976164a
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-05-18 20:39:39 +0800

    Update UserLayout.jsx

--- resources/js/Layouts/UserLayout.jsx
+++ resources/js/Layouts/UserLayout.jsx
@@ -1,19 +1,24 @@
 import { MantineProvider } from "@mantine/core";
+import { usePage } from "@inertiajs/react";
+
 import NavHeader from "../Components/NavHeader";
 import Footer from "../Components/Footer";
 
-
 export default function UserLayout({ children }) {
+    const { url } = usePage();
+    const hideFooter = url.startsWith("/login") || url.startsWith("/register");
     return (
         <MantineProvider>
             <header>
                 <NavHeader />
             </header>
 
-        <main>{children}</main>
-<footer><Footer/></footer>
+            <main>{children}</main>
+            {!hideFooter && (
+                <footer>
+                    <Footer />
+                </footer>
+            )}
         </MantineProvider>
     );
 }
-
-
