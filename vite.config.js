b426b568600c34c52cc99bcb5405e0cec66c72fa
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-06-04 22:53:28 +0800

    updated fonts

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,37 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/js/app.jsx'],
-      refresh: true,
-    }),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/js'),
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
-    },
-  },
-  build: {
-    manifest: true,
-    outDir: 'public/build',
-    emptyOutDir: true,
-    sourcemap: false,
-    assetsDir: '.',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (/\.(ttf|woff2?|eot|otf)$/.test(assetInfo.name)) {
-            return 'fonts/[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
+    plugins: [
+        laravel({
+            input: ["resources/js/app.jsx"],
+            refresh: true,
+        }),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+            "@tabler/icons-react":
+                "@tabler/icons-react/dist/esm/icons/index.mjs",
         },
-      },
     },
-  },
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        emptyOutDir: true,
+        assetsDir: ".",
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (/\.(ttf|woff2?|eot|otf)$/.test(assetInfo.name)) {
+                        return "fonts/[name][extname]";
+                    }
+                    return "assets/[name]-[hash][extname]";
+                },
+            },
+        },
+    },
 });
