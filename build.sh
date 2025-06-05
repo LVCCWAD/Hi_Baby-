81c852dbdf650108be4787808a8ca2dacb632c06
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-06-05 22:14:03 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get install -y libpq-dev
-docker-php-ext-install pdo pdo_pgsql
-
-composer install --no-dev --optimize-autoloader
-php artisan config:clear
-php artisan migrate --force
