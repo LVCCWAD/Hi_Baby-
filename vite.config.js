64216a2f113ba9aba16bebb220185ea1723cf505
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-06-05 20:10:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,24 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-    base:
-        mode === "production"
-            ? "https://hibaby-production-0186.up.railway.app/build/"
-            : "/",
+
+export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.jsx"],
+            input: ['resources/js/app.jsx'],
             refresh: true,
-            manifest: "manifest.json",
         }),
+        tailwindcss(),
         react(),
     ],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "resources/js"),
-            "@tabler/icons-react":
-                "@tabler/icons-react/dist/esm/icons/index.mjs",
+            '@': '/resources/js',
+            '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+
         },
     },
-    build: {
-        manifest: "manifest.json",
-        outDir: "public/build",
-        emptyOutDir: true,
-        sourcemap: false,
-        cssCodeSplit: false,
-        chunkSizeWarningLimit: 2000,
-        assetsDir: ".",
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    if (/\.(ttf|woff2?|eot|otf)$/.test(assetInfo.name)) {
-                        return "fonts/[name][extname]";
-                    }
-                    return "assets/[name]-[hash][extname]";
-                },
-            },
-        },
-    },
-}));
+
+});
