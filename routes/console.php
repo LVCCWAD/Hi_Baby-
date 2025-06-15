8944ccbd63c78ba09bd0b6cbb7f95fadd1511f52
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-06-15 10:52:54 +0800

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('orders:complete')->everyMinute();
+Schedule::command('orders:complete')->cron('0 0 */3 * *');
